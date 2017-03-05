685c1a58771794869610fc329110cc3d0a42baec
switzer40 <switzer40@googlemail.com>
2017-03-05 05:05:04 +0100

    Getting started
    Unfortunately this solution does not compile due to problems with
    bundling. I just wanted to get it into  source control so we could work
    on the problems together

---
+++ PolishVerbLoader/PolishVerbLoader.Api/Models/PrimiiveVerbDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PolishVerbLoader.Api.Models
+{
+    public class PrimiiveVerbDto : BaseEntityDto
+    {
+        public string Imperfective { get; set; }
+        public string Perfective { get; set; }
+        public bool Reflexive { get; set; }
+        public int ConjugationClass { get; set; }
+        public string PStem { get; set; }
+        public string VStem { get; set; }
+    }
+}
---
+++ PolishVerbLoader/PolishVerbLoader.Core/Entities/PrimitiveVerb.cs
@@ -0,0 +1,19 @@
+﻿using PolishVerbLoader.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolishVerbLoader.Core.Entities
+{
+    public class PrimitiveVerb : BaseEntity
+    {
+        public string Imperfective { get; set; }
+        public string Perfective { get; set; }
+        public bool Reflexive { get; set; }
+        public int ConjugationClass { get; set; }
+        public string PStem { get; set; }
+        public string VStem { get; set; }
+    }
+}
